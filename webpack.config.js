55a0940b094b0e73e964dd52cfda975bd61c94c6
Ashley Karhoff <ashley.karhoff1@gmail.com>
2021-01-22 21:09:09 -0800

    Configured webpack and installed basic dependencies

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: __dirname + "/build",
+    filename: "index.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
